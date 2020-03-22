180ae7aa1ac2a15042dad85f3082e22535f94385
Thomas W <tomw1808@gmail.com>
2020-03-22 07:01:25 +0100

    added all files

---
+++ index.php
@@ -0,0 +1,6 @@
+<?php
+
+
+$hostname = @file_get_contents('http://169.254.169.254/latest/meta-data/public-hostname/');
+$hostname = $hostname == "" ? "Not an AWS instance" : $hostname;
+echo "Output 2: ".$hostname;
