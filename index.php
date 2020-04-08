214136850c9603d4112a5698d3952b7c70abd754
Thomas W <tomw1808@gmail.com>
2020-04-08 07:04:40 +0200

    updated

--- index.php
+++ index.php
@@ -1,6 +1,7 @@
 <?php
 
 
+
 $hostname = @file_get_contents('http://169.254.169.254/latest/meta-data/public-hostname/');
 $hostname = $hostname == "" ? "Not an AWS instance" : $hostname;
 echo "Output after update 2: ".$hostname;
